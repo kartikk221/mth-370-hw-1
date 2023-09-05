63b84866c836f0798dfb885d71d22cbed6019cc0
kartikk221 <kartikk221@yahoo.com>
2023-09-04 21:26:03 -0400

    Completed

--- largest_in_minute.py
+++ largest_in_minute.py
@@ -1,4 +1,8 @@
+import sys
 import time
+
+# We need to do this because output factorial is very large
+sys.set_int_max_str_digits(100000000)
 
 n = 0
 factorial = 1
@@ -19,4 +23,6 @@ while True:
         break
 
 # Print the result
-print(f"Largest computable factorial in under 1 minute: {n - 1}")
+print(f"Largest computable factorial in under 1 minute: {n - 1}!")
+print(f"Time elapsed: {time.time() - start_time} seconds")
+print(f"{n - 1}! = a number that is {len(str(factorial))} digits long")
